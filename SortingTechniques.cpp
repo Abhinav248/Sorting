47f447879e273561bd4569cac150f269aaf671c1
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2018-04-29 11:17:04 +0530

    Update SortingTechniques.cpp

--- SortingTechniques.cpp
+++ SortingTechniques.cpp
@@ -7,16 +7,16 @@ This contains various sorting techniques using switch case.
 using namespace std;
 
 /*
-Algorithm	                 Time Complexity
-                    Best	     Average	    Worst	
+Algorithm	                  Time Complexity
+                    Best	       Average	      Worst	
 Selection Sort	    Ω(n^2)	     θ(n^2)	        O(n^2)
-Bubble Sort	        Ω(n)	     θ(n^2)	        O(n^2)
-Insertion Sort	    Ω(n)	     θ(n^2)	        O(n^2)
-Heap Sort	        Ω(n log(n))	 θ(n log(n))	O(n log(n))
+Bubble Sort	        Ω(n)	       θ(n^2)	        O(n^2)
+Insertion Sort	    Ω(n)	       θ(n^2)	        O(n^2)
+Heap Sort	          Ω(n log(n))	 θ(n log(n))	  O(n log(n))
 Quick Sort	        Ω(n log(n))	 θ(n log(n))    O(n^2)
-Merge Sort	        Ω(n log(n))	 θ(n log(n))	O(n log(n))
+Merge Sort	        Ω(n log(n))	 θ(n log(n))	  O(n log(n))
 Bucket Sort	        Ω(n+k)	     θ(n+k)	        O(n^2)
-Radix Sort	        Ω(nk)	     θ(nk)	        O(nk)
+Radix Sort	        Ω(nk)	       θ(nk)	        O(nk)
 */
 
 
